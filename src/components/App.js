e24ee5bedb16a2b8118608ea3ab5c287f288bbe6
CahangirAsgerov <esgerovcahangir12@gmail.com>
2023-08-31 22:50:12 +0400

    Sidebar component  will be added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./Header";
 import Home from "./Home";
 import NotFound from "./NotFound";
+import SideBar from "./SideBar";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<Home />}></Route>
-          </Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+      <div className="d-flex">
+        <SideBar />
+
+        <div className="routerContent">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/SideBar.js
@@ -0,0 +1,64 @@
+import { NavLink } from "react-router-dom";
+import "./sideBar.css";
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div
+      className="d-flex flex-column bg-dark p-3 text-white flex-shrink-0"
+      style={{ width: 280 + "px", height: 100 + "%" }}
+    >
+      <div className="d-flex align-items-center">
+        <img
+          alt="CK-logo"
+          src={process.env.PUBLIC_URL + "/images/CK_logo.jpg"}
+          style={{ width: 40 + "px", borderRadius: 5 + "px" }}
+        />
+        <h1 className="brandTitle">Mech Traders</h1>
+      </div>
+
+      <hr></hr>
+
+      <ul className="nav nav-pills flex-column mb-auto navLinks">
+        <li className="nav-item">
+          <NavLink to="/">
+            <i className="bi bi-house-door-fill me-2"></i>Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/c">
+            <i className="bi bi-receipt me-2"></i>Cars for Sale
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/b">
+            <i className="bi bi-binoculars me-2"></i>Research & Reviews
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/d">
+            <i className="bi bi-camera-reels me-2"></i>News & Videos
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/e">
+            <i className="bi bi-car-front me-2"></i>Sell Your Car
+          </NavLink>
+        </li>
+      </ul>
+
+      <hr></hr>
+
+      <div className="d-flex align-items-center">
+        <img
+          alt="Businnesman"
+          src={process.env.PUBLIC_URL + "/images/businessman.png"}
+          style={{ width: 40 + "px", borderRadius: 5 + "px" }}
+          className="me-2"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
